77a24e63e5775edf71905865c6f3fb8b00fad76d
清樨 <11401187781@qq.com>
2018-09-12 02:44:03 +0800

    work

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int m = 0;
+            int n = 0;
+            Console.WriteLine("Please input an int : ");
+            s = Console.ReadLine();
+            m = int.Parse(s);
+            Console.WriteLine("Please input another int : ");
+            s = Console.ReadLine();
+            n = int.Parse(s);
+            Console.WriteLine("Product of the two numbers is : " + (m * n));
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            double m = 0;
+            double n = 0;
+            double p = 0;
+            s = textBox1.Text;
+            m = double.Parse(s);
+            s = textBox2.Text;
+            n = double.Parse(s);
+            p = m * n;
+            label3.Text = "Product of two numbers is : " + p;
+        }
+    }
+}
