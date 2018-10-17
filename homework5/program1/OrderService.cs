8ec6f8426fff35eea6082c398384c639cb2fcf33
清樨 <11401187781@qq.com>
2018-10-17 20:22:43 +0800

    work4

---
+++ homework5/program1/OrderService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class OrderService
+    {
+        private Dictionary<uint, Order> orderDict;
+
+        public OrderService()
+        {
+            orderDict = new Dictionary<uint, Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            if (orderDict.ContainsKey(order.Id))
+                throw new Exception($"order-{order.Id} is already existed!");
+            orderDict[order.Id] = order;
+        }
+
+        public void RemoveOrder(uint orderId)
+        {
+            orderDict.Remove(orderId);
+        }
+
+        public List<Order> QueryAllOrders()
+        {
+            return orderDict.Values.ToList();
+        }
+
+        public Order GetById(uint orderId)
+        {
+            return orderDict[orderId];
+        }
+
+        public List<Order> QueryByGoodsName(string goodsName)
+        {
+            List<Order> result = new List<Order>();
+            foreach (Order order in orderDict.Values)
+            {
+                foreach (OrderDetail detail in order.Details)
+                {
+                    if (detail.Goods.Name == goodsName)
+                    {
+                        result.Add(order);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<Order> QueryByCustomerName(string customerName)
+        {
+            List<Order> orders = orderDict.Values.ToList();
+            var query = from s in orders
+                        where s.Customer.Name == customerName
+                        select s;
+            return query.ToList();
+        }
+
+        public void UpdateCustomer(uint orderId, Customer newCustomer)
+        {
+            if (orderDict.ContainsKey(orderId))
+            {
+                orderDict[orderId].Customer = newCustomer;
+            }
+            else
+            {
+                throw new Exception($"order-{orderId} is not existed!");
+            }
+        }
+    }
+}
